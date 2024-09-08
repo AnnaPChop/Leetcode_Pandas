9a23dc569cc1f87d12c4c2c612e4f5d015c7ae2f
AnnaPChop <annapicn@gmail.com>
2024-09-07 23:40:10 -0600

    Update and rename easy_pandas2 to easy_pandas2.py

---
+++ easy_pandas2.py
@@ -0,0 +1,27 @@
+'''2878. Get the Size of a DataFrame
+
+DataFrame players:
++-------------+--------+
+| Column Name | Type   |
++-------------+--------+
+| player_id   | int    |
+| name        | object |
+| age         | int    |
+| position    | object |
+| ...         | ...    |
++-------------+--------+
+Write a solution to calculate and display the number of rows and columns of players.
+
+Return the result as an array:
+
+[number of rows, number of columns]'''
+
+# Solution:
+
+import pandas as pd
+
+def getDataframeSize(players: pd.DataFrame) -> List[int]:
+    [r,c] = players.shape
+    return [r,c]
+
+
